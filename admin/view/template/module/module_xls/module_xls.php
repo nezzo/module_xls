3adfa1f383b82495194a9800dd58888eabe8e116
nezzo <lestatx@yandex.ru>
2016-10-10 01:56:47 +0300

    09.10.2016

--- admin/view/template/module/module_xls/module_xls.php
+++ admin/view/template/module/module_xls/module_xls.php
@@ -17,13 +17,14 @@ class Module_xls {
    
    /*Принимаем данные и записываем в массив*/
    function module_write_xls(){
-        $mas = $_POST['mas'];
-       
-        var_dump($mas);
-        
-        
-       // Создаем объект класса PHPExcel
-       $xls = new PHPExcel();
+        $product_id = $_POST['product_id'];
+        $quantity = $_POST['quantity'];
+        $total = $_POST['total'];
+        $rows_table = $_POST['rows_table'];
+        $mas= array($product_id,$quantity,$total);
+
+        // Создаем объект класса PHPExcel
+        $xls = new PHPExcel();
        //Открываем файл-шаблон
         $objReader = PHPExcel_IOFactory::createReader('Excel5');
         $xls = $objReader->load('PHPExcel/import.xls');
@@ -36,27 +37,27 @@ class Module_xls {
         
         
         /*Создаем цыкл для массива и заполняем все оставшиеся поля*/
-       for ($i = 0; $i< count($product_id); $i++){
+       for ($i = 0; $i< $rows_table; $i++){
            $index = 2 + $i;
            
           // (id_Product)
         $sheet->setCellValue('A'.$index, $mas[0][$i]);
         $sheet->getStyle('A'.$index)->getFill()->setFillType(
-            PHPExcel_Style_Fill::FILL_SOLID);
+            PHPExcel_Style_Border::BORDER_THIN);
         $sheet->getStyle('A'.$index)->getFill()->getStartColor()->setRGB('EEEEEE');
        
          // (Количество)
         $sheet->setCellValue('B'.$index, $mas[1][$i]);
         $sheet->getStyle('B'.$index)->getFill()->setFillType(
-            PHPExcel_Style_Fill::FILL_SOLID);
+            PHPExcel_Style_Border::BORDER_THIN);
         $sheet->getStyle('B'.$index)->getFill()->getStartColor()->setRGB('EEEEEE');
         
          // (Сумма)
         $sheet->setCellValue('C'.$index, $mas[2][$i]);
         $sheet->getStyle('C'.$index)->getFill()->setFillType(
-            PHPExcel_Style_Fill::FILL_SOLID);
+            PHPExcel_Style_Border::BORDER_THIN);
         $sheet->getStyle('C'.$index)->getFill()->getStartColor()->setRGB('EEEEEE');
-        
+        }
                
         
         /*Сохраняем данные в файл (путь/файл) и скачиваем*/
@@ -69,10 +70,10 @@ class Module_xls {
          
         /*передаем с помощью GET запроса на скрипт для скачивания отчета*/
          if($new_name == true){
-                echo "view/template/module/module_xls/downoload_script_otchet/downoload.php?file=../otchet/import(09.10.2016).xls";
+                echo "view/template/module/module_xls/downoload_script_otchet/downoload.php?file=../otchet/import($data).xls";
          }
          
-    }
+    
 }
 }
 
